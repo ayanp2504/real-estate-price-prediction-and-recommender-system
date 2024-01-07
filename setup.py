f136a120d3a1216739e20cb4bb6380a74b95444e
Ayan Pinjara <ayanpinjara@gmail.com>
2024-01-07 16:23:56 -0600

    Web scraping code added and data is scraped

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project we will predict housing prices and recommend apartments to users',
+    author='Ayan Pinjara',
+    license='',
+)
